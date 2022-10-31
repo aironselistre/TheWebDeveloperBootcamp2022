cddda1664d07605afe40a969c764504664545549
aironselistre <airon.selistre@gmail.com>
2022-10-31 02:24:17 -0300

    YelpCamp project

---
+++ Projects/YelpCamp/seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDb = async () => {
+  await Campground.deleteMany({});
+  for (let index = 0; index < 50; index++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequatur ex quidem ipsam quaerat commodi maxime autem quas delectus dignissimos ea nihil eaque voluptatibus alias veniam!",
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDb().then(() => {
+  mongoose.connection.close();
+});
